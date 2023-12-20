79465f446faf266b80d395bfdb6a84484389e9ac
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-12-20 18:32:07 -0300

    :rocket: Desafio finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new("40028922", "Tijolão", "22982004", 24);
+Iphone iphone = new("12345678", "iPobre 14", "87654321", 24);
+nokia.InstalarAplicativo("Jogo da Cobrinha");
+iphone.InstalarAplicativo("Sirene");
